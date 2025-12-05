d37f63f5f16f5cc7b3d381b344c604cc08fcde19
pedrocortes <p.cortes.nieves@gmail.com>
2025-12-05 07:36:06 +0100

    Config the jest environment for TDD

---
+++ frontend/src/__test__/suma.test.js
@@ -0,0 +1,6 @@
+// test
+const sum = require('../suma');
+
+test('add 1 to 2 gives me 3', () => {
+    expect(sum(1, 2)).toBe(3);
+});
---
+++ frontend/src/suma.js
@@ -0,0 +1,6 @@
+// function to test
+function sum(a, b)
+{
+    return a + b;
+}
+module.exports = sum;
